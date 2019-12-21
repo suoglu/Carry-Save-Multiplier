5cd87e4f8c1ddfd58d8ae6ebe279c1b6a3ee7bfb
suoglu <suoglu@icloud.com>
2019-12-21 12:08:36 +0100

    binary control signals for easier result check added

--- testbench.v
+++ testbench.v
@@ -3,6 +3,7 @@
 module tb();
     reg [3:0] num1, num2, num3, num4, num5, num6, num7, num8, dummy;
     wire [8:0] result1, result2, result3, result4;
+    wire binary_result1, binary_result2, binary_result3,  binary_result4;
 
     multiCS4 uut1(num1, num2, result1);
     multiCS4 uut2(num3, num4, result2);
@@ -19,7 +20,12 @@ module tb();
            num6 <= 4'd14;
            num7 <= 4'd6;
            num8 <= 4'd6;
-           #1000
+           #100
+           binary_result1 <= (result1 == 9'd12);
+           binary_result2 <= (result2 == 9'd70);
+           binary_result3 <= (result3 == 9'd126);
+           binary_result4 <= (result4 == 9'd36);
+           #100
            dummy =  0;
         end
 endmodule // tb
