b29adbf89ec89cb4459e0209223b3d8b9f64af31
suoglu <suoglu@icloud.com>
2019-12-22 11:16:00 +0100

    additions for basecell version

--- testbench.v
+++ testbench.v
@@ -2,29 +2,39 @@
 
 module tb();
     reg [3:0] num1, num2, num3, num4, num5, num6, num7, num8, dummy;
-    wire [8:0] result1, result2, result3, result4;
-    wire binary_result1, binary_result2, binary_result3,  binary_result4;
+    wire [8:0] result1, result2, result3, result4, result5, result6, result7, result8;
+    reg [7:0] binary_result;
 
-    multiCS4 uut1(num1, num2, result1);
-    multiCS4 uut2(num3, num4, result2);
-    multiCS4 uut3(num5, num6, result3);
-    multiCS4 uut4(num7, num8, result4);
+    multiCS4_v1 uut1(num1, num2, result1);
+    multiCS4_v1 uut2(num3, num4, result2);
+    multiCS4_v1 uut3(num5, num6, result3);
+    multiCS4_v1 uut4(num7, num8, result4);
+
+    multiCS4_fullbasecell uut5(num1, num2, result5);
+    multiCS4_fullbasecell uut6(num3, num4, result6);
+    multiCS4_fullbasecell uut7(num5, num6, result7);
+    multiCS4_fullbasecell uut8(num7, num8, result8);
 
     initial //test cases here
         begin
-            num1 <= 4'd3;
-            num2 <= 4'd4;
-            num3 <= 4'd10;
-            num4 <= 4'd7;
-           num5 <=  4'd9;
+           num1 <= 4'd3;
+           num2 <= 4'd4;
+           num3 <= 4'd10;
+           num4 <= 4'd7;
+           num5 <= 4'd9;
            num6 <= 4'd14;
            num7 <= 4'd6;
            num8 <= 4'd6;
            #100
-           binary_result1 <= (result1 == 9'd12);
-           binary_result2 <= (result2 == 9'd70);
-           binary_result3 <= (result3 == 9'd126);
-           binary_result4 <= (result4 == 9'd36);
+           binary_result[0] <= (result1 == 9'd12);
+           binary_result[1] <= (result2 == 9'd70);
+           binary_result[2] <= (result3 == 9'd126);
+           binary_result[3] <= (result4 == 9'd36);
+
+           binary_result[4] <= (result1 == 9'd12);
+           binary_result[5] <= (result2 == 9'd70);
+           binary_result[6] <= (result3 == 9'd126);
+           binary_result[7] <= (result4 == 9'd36);
            #100
            dummy =  0;
         end
